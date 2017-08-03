e81a081605684c79d48909b2fc270e225645b897
Sruc73 <michaelmares@outlook.com>
2017-08-03 18:07:27 +0200

    Added data directory and paste picture in it

--- pygame.py
+++ pygame.py
@@ -1,11 +1,22 @@
 import pygame
 from pygame.locals import *
 
-
 pygame.init()
-fenetre = pygame.display.set_mode((640, 480))
 
+#ouverture de la fenêtre pygame
+fenetre = pygame.display.set_mode((640, 480), RESIZABLE)
+
+#Chargement et collage du fond
+fond = pygame.image.load("data/background.jpg").convert()
+fenetre.blit(fond, (0, 0))
+
+perso = pygame.image.load("data/perso.png").convert_alpha()
+fenetre.blit(perso, (200, 300))
+
+#Rafraîchissement de l'écran
+pygame.display.flip()
+
+#Boucle infinie
 continuer = 1
-
 while continuer == 1:
-    continue
+    continuer = int(input())
